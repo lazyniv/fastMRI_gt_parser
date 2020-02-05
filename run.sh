b18e22957de87c0c3981e3e708be57a5188f45ba
Dmitry Vlasov <lazyniv@gmail.com>
2020-02-05 22:50:04 +0700

    fix run.sh and set more sleep time for event handler

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python3 src/hdf5_watcher.py $1 &
-curl -C - $1 | tar xvz -C $2
+python3 src/hdf5_watcher.py "$2" &
+curl -C - "$1" | tar xvz -C "$2"
 
--- src/events_handler.py
+++ src/events_handler.py
@@ -6,22 +6,24 @@ from watchdog.events import RegexMatchingEventHandler
 
 class HDF5EventHandler(RegexMatchingEventHandler):
 
-    HDF5_REGEX = [r".*[^_thumbnail]\.h5$"]
+    HDF5_REGEX = [r".*\.h5$"]
 
     def __init__(self):
         super().__init__(self.HDF5_REGEX)
 
     def on_created(self, event):
         file_size = -1
+
         while file_size != os.path.getsize(event.src_path):
             file_size = os.path.getsize(event.src_path)
-            time.sleep(1)
+            time.sleep(30)
 
         self.process(event)
 
     def process(self, event):
         filename, ext = os.path.splitext(event.src_path)
         filename = "{filename}_gt.h5".format(filename=filename)
+
         with h5py.File(event.src_path, "r") as src:
             with h5py.File(filename, "w") as target:
                 gt_tensor = src["reconstruction_rss"][()]
